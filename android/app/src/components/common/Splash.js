3a21432740663bc92788e378b56cfe1cb9b0ae05
Amira <amira_atef20@hotmail.com>
2019-05-08 18:45:07 +0200

    'splash-screen'

--- android/app/src/components/common/Splash.js
+++ android/app/src/components/common/Splash.js
@@ -1,61 +1,21 @@
 import React, { Component } from "react";
-import { View, Image, ScrollView, TouchableOpacity } from "react-native";
-import ImageOverlay from "react-native-image-overlay";
+import { View, ImageBackground } from "react-native";
 import { Text, Button } from "native-base";
 import {
   responsiveHeight,
   responsiveWidth
 } from "react-native-responsive-dimensions";
-import {GlobalStyles} from '../globalStyles/globalStyles'
 export default class SplashSign extends Component {
-  state = {};
-Login=()=>{
-  this.props.history.push('/Login')
-}
-
-SignUp=()=>{
-  this.props.history.push('/SignUp')
-}
 
   render() {
     return (
-      <ImageOverlay
-        source={require("../../main/assets/Images/BG.png")}
-        containerStyle={{ height: responsiveHeight(100) }}
-        resizeMode="cover"
-      >
-        <Image
-          style={{
-            width: "40%",
-            height: "25%",
-            top: responsiveHeight(15),
-            position: "absolute"
-          }}
-          source={require("../../main/assets/Images/logo.png")}
-        />
-        <Text
-          style={{
-            color: "white",
-            marginTop: responsiveHeight(40),
-            fontSize: 12
-          }}
-        >
-          Don't have account
-        </Text>
+<ImageBackground
 
-        <Button
-                onPress={()=>{this.SignUp()}}
+style={{width:responsiveWidth(100),height:responsiveHeight(100)}}
+source={require('../../main/Images/splash.png')}
+/>
 
-         rounded block style={{ backgroundColor:GlobalStyles.mainColor, margin: responsiveHeight(1) }}>
-          <Text uppercase={false}>Sign up</Text>
-        </Button>
 
-        <Button 
-        onPress={()=>{this.Login()}}
-        rounded block style={{ backgroundColor:GlobalStyles.mainColor, margin: responsiveHeight(1) }}>
-          <Text uppercase={false}>Login</Text>
-        </Button>
-      </ImageOverlay>
     );
   }
 }
--- routes.js
+++ routes.js
@@ -1,28 +1,40 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { NativeRouter, Route, Link ,Switch } from "react-router-native";
-import Home from './android/app/src/components/Home/Home';
-import Map from './android/app/src/components/Map/Map';
-import NewTask from './android/app/src/components/OrderDetails/NewTask'
+import Splash from './android/app/src/components/common/Splash'
+import { NativeRouter, Route, Link, Switch } from "react-router-native";
+import Home from "./android/app/src/components/Home/Home";
+import MapApp from "./android/app/src/components/Map/Map";
+import NewTask from "./android/app/src/components/OrderDetails/NewTask";
 import { GlobalStyles } from "./android/app/src/globalStyles/globalStyles";
 import { responsiveHeight } from "react-native-responsive-dimensions";
 class Routes extends Component {
 
+  state = {
+    currentComponent:Splash,
+  };
+
+  switch=()=>{ setTimeout(
+    () => {
+    this.setState({currentComponent:NewTask})
+  }, 2000)
+}
+componentDidMount(){
+  this.switch()
+}
   render() {
-  
     return (
       <View
-      style={{backgroundColor:GlobalStyles.mainColor,height:responsiveHeight(100)}}
+        style={{
+          backgroundColor: GlobalStyles.mainColor,
+          height: responsiveHeight(100)
+        }}
       >
-       
-      <NativeRouter
-    
-      >
-            <Route exact path="/" component={NewTask} />
-              <Route exact path="/Map" component={Map} />
-      </NativeRouter>
+        <NativeRouter>
+          <Route exact path="/" component={this.state.currentComponent} />
+          <Route exact path="/NewTask" component={NewTask} />
+          <Route exact path="/Map" component={MapApp} />
+        </NativeRouter>
       </View>
-
     );
   }
 }
